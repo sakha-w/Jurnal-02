02e21b5fb8362c4d2bc0b8a6df7943958649b2de
unknown <sakhawibisono@student.telkomuniversity.ac.id>
2021-09-28 22:18:12 +0700

    HelloWorld.java

---
+++ Jurnal 02/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+	
+	public static void main(String[] args) {
+		System.out.println("Nama : Sakha Wibisono");
+		System.out.println("NIM  : 6706213102");
+		System.out.println("Kelas: 45-01");
+	}
+}
